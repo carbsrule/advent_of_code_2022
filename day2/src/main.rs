e7ca85926dc21536ca4905ad4fd8962a1e1dd404
Benno Lang <transmogribenno@gmail.com>
2022-12-03 00:13:03 +1030

    AOC day 2, part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::io;
 use std::process;
 use substring::Substring;
@@ -21,6 +22,32 @@ fn calc_score(letter: &str) -> u32 {
     }
 }
 
+fn calc_response(opp: u32, my: &str) -> u32 {
+    // X: lose, Y: draw, Z: win
+    if my == "Y" {
+        return opp;
+    }
+    if opp == ROCK {
+        match my {
+            "X" => SCISSORS,
+            "Z" => PAPER,
+            &_ => process::exit(2),
+        }
+    } else if opp == PAPER {
+        match my {
+            "X" => ROCK,
+            "Z" => SCISSORS,
+            &_ => process::exit(3),
+        }
+    } else { // opp == SCISSORS
+        match my {
+            "X" => PAPER,
+            "Z" => ROCK,
+            &_ => process::exit(4),
+        }
+    }
+}
+
 fn calc_result(opp: u32, my: u32) -> [u32; 2] {
     if opp == my {
         return [3, 3];
@@ -39,6 +66,7 @@ fn calc_result(opp: u32, my: u32) -> [u32; 2] {
 }
 
 fn main() {
+    let part = env::args().nth(1).unwrap_or("1".to_string());
     let mut my_total = 0;
     loop {
         let mut line = String::new();
@@ -55,7 +83,12 @@ fn main() {
         println!("{opp_letter} vs {my_letter}");
 
         let opp_score = calc_score(opp_letter);
-        let my_score = calc_score(my_letter);
+        let my_score;
+        if part == "2" {
+            my_score = calc_response(opp_score, my_letter);
+        } else {
+            my_score = calc_score(my_letter);
+        }
 
         let mut result = calc_result(opp_score, my_score);
         result[0] += opp_score;
