feb83d578b950c8408b0602700e2bb51c1c68054
Benno Lang <transmogribenno@gmail.com>
2022-12-06 17:15:16 +1030

    AOC day 6, part 2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,17 +1,17 @@
-const START_PACKET_LEN: usize = 4;
+const BUFFER_LEN: usize = 14;
 
-fn rearrange(chars: &mut [char; START_PACKET_LEN]) {
-    for i in 0..START_PACKET_LEN - 1 {
+fn rearrange(chars: &mut [char; BUFFER_LEN], len: usize) {
+    for i in 0..len - 1 {
         chars[i] = chars[i + 1];
     }
 }
 
-fn has_dup(chars: &[char; START_PACKET_LEN]) -> bool {
+fn has_dup(chars: &[char; BUFFER_LEN], len: usize) -> bool {
     if chars[0] == ' ' {
         return true;
     }
-    for j in 0..START_PACKET_LEN {
-        for k in j+1..START_PACKET_LEN {
+    for j in 0..len {
+        for k in j+1..len {
             if chars[j] == chars[k] {
                 return true;
             }
@@ -21,6 +21,8 @@ fn has_dup(chars: &[char; START_PACKET_LEN]) -> bool {
 }
 
 fn main() {
+    let part = base::get_part();
+    let check_len: usize = if part == 2 { 14 } else { 4 };
     loop {
         let (num_bytes, line) = base::read_line();
         if num_bytes == 0 {
@@ -28,14 +30,14 @@ fn main() {
         }
 
         let mut i = 0;
-        let mut chars: [char; 4] = [' ', ' ', ' ', ' '];
+        let mut chars: [char; 14] = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
         for ch in line.chars() {
-            rearrange(&mut chars);
-            chars[START_PACKET_LEN-1] = ch;
+            rearrange(&mut chars, check_len);
+            chars[check_len -1] = ch;
             // println!("Rearranged packet: {:?} at pos {}", chars, i);
 
-            if !has_dup(&chars) {
-                println!("Start packet: {:?}; message at pos {}", chars, i+1);
+            if !has_dup(&chars, check_len) {
+                println!("Start packet: {:?}; packet at pos {}", chars, i+1);
                 break;
             }
             i += 1;
